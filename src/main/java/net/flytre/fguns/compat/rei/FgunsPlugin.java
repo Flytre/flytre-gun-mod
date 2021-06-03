a479fb82b6b8e352f11a89e956a2b8061b0dfb85
shedaniel <daniel@shedaniel.me>
2021-06-03 14:23:24 +0800

    Update to REI 6

--- src/main/java/net/flytre/fguns/compat/rei/FgunsPlugin.java
+++ src/main/java/net/flytre/fguns/compat/rei/FgunsPlugin.java
@@ -1,32 +1,29 @@
 package net.flytre.fguns.compat.rei;
 
 import me.shedaniel.math.Rectangle;
-import me.shedaniel.rei.api.BaseBoundsHandler;
-import me.shedaniel.rei.api.DisplayHelper;
-import me.shedaniel.rei.api.EntryStack;
-import me.shedaniel.rei.api.RecipeHelper;
-import me.shedaniel.rei.api.plugins.REIPluginV0;
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.client.registry.screen.ExclusionZones;
+import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.flytre.fguns.FlytreGuns;
 import net.flytre.fguns.compat.rei.categories.WorkbenchRecipeCategory;
 import net.flytre.fguns.compat.rei.displays.WorkbenchRecipeDisplay;
+import net.flytre.fguns.workbench.WorkbenchRecipe;
 import net.flytre.fguns.workbench.WorkbenchScreen;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.item.ItemConvertible;
-import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeType;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 import java.util.*;
-import java.util.function.Function;
 
 @Environment(EnvType.CLIENT)
-public class FgunsPlugin implements REIPluginV0 {
+public class FgunsPlugin implements REIClientPlugin {
 
-    public static final Identifier PLUGIN = new Identifier("fguns", "plugin");
     public static final Map<RecipeType<?>, ItemConvertible> ICON_MAP = new HashMap<>();
     public static final List<RecipeType<?>> types = new ArrayList<>();
 
@@ -36,43 +33,26 @@ public class FgunsPlugin implements REIPluginV0 {
     }
 
     @Override
-    public Identifier getPluginIdentifier() {
-        return PLUGIN;
-    }
-
-    @Override
-    public void registerBounds(DisplayHelper displayHelper) {
-        BaseBoundsHandler baseBoundsHandler = BaseBoundsHandler.getInstance();
-        baseBoundsHandler.registerExclusionZones(WorkbenchScreen.class, () ->
+    public void registerScreens(ScreenRegistry registry) {
+        ExclusionZones exclusionZones = registry.exclusionZones();
+        exclusionZones.register(WorkbenchScreen.class, screen ->
         {
-            Screen currentScreen = MinecraftClient.getInstance().currentScreen;
-            List<Rectangle> result = new ArrayList<>();
-            if (currentScreen instanceof WorkbenchScreen) {
-                WorkbenchScreen actualScreen = (WorkbenchScreen) currentScreen;
-                int x = actualScreen.getX();
-                int y = actualScreen.getY();
-                result.add(new Rectangle(x - 205, y, 500, 167));
-            }
-            return result;
+            int x = screen.getX();
+            int y = screen.getY();
+            return Collections.singletonList(new Rectangle(x - 205, y, 500, 167));
         });
     }
 
     @Override
-    public void registerPluginCategories(RecipeHelper recipeHelper) {
-        recipeHelper.registerCategory(new WorkbenchRecipeCategory(FlytreGuns.WORKBENCH_RECIPE));
-    }
-
-    @Override
-    public void registerRecipeDisplays(RecipeHelper recipeHelper) {
-        recipeHelper.registerRecipes(Registry.RECIPE_TYPE.getId(FlytreGuns.WORKBENCH_RECIPE), (Function<Recipe, Boolean>) recipe -> recipe.getType() == FlytreGuns.WORKBENCH_RECIPE, WorkbenchRecipeDisplay::new);
-    }
-
-
-    @Override
-    public void registerOthers(RecipeHelper recipeHelper) {
+    public void registerCategories(CategoryRegistry registry) {
+        registry.add(new WorkbenchRecipeCategory(FlytreGuns.WORKBENCH_RECIPE));
         for (RecipeType<?> type : types)
-            recipeHelper.registerWorkingStations(Registry.RECIPE_TYPE.getId(type), EntryStack.create(ICON_MAP.get(type)));
+            registry.addWorkstations(CategoryIdentifier.of(Registry.RECIPE_TYPE.getId(type)), EntryStacks.of(ICON_MAP.get(type)));
     }
 
+    @Override
+    public void registerDisplays(DisplayRegistry registry) {
+        registry.registerFiller(WorkbenchRecipe.class, recipe -> recipe.getType() == FlytreGuns.WORKBENCH_RECIPE, WorkbenchRecipeDisplay::new);
+    }
 
 }
